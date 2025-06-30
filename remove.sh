b7c5a6dc6f6171d5ce135b3493a0056d65322b24
davidattip <davidkodjoattipoupou@gmail.com>
2025-06-30 10:05:50 +0200

    petite correction

--- remove.sh
+++ remove.sh
@@ -9,18 +9,21 @@ sudo systemctl stop ssh_honeypy
 sudo systemctl stop email_honeypy
 sudo systemctl stop malware_honeypy
 sudo systemctl stop honeypy_dashboard
+sudo systemctl stop web_honeypy
 
 echo "[+] Désactivation des services..."
 sudo systemctl disable ssh_honeypy
 sudo systemctl disable email_honeypy
 sudo systemctl disable malware_honeypy
 sudo systemctl disable honeypy_dashboard
+sudo systemctl disable web_honeypy
 
 echo "[+] Suppression des fichiers systemd..."
 sudo rm -f /etc/systemd/system/ssh_honeypy.service
 sudo rm -f /etc/systemd/system/email_honeypy.service
 sudo rm -f /etc/systemd/system/malware_honeypy.service
 sudo rm -f /etc/systemd/system/honeypy_dashboard.service
+sudo rm -f /etc/systemd/system/web_honeypy.service
 
 echo "[+] Nettoyage systemd..."
 sudo systemctl daemon-reload
